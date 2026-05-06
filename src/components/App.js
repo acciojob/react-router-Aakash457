4ba2c1d3d010e09c2d8ba5fe5b1d92387c69d2d0
Aakash457 <114485477+Aakash457@users.noreply.github.com>
2026-05-07 00:18:55 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,18 @@
 import React from "react";
-import './../styles/App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
+import Navigation from "./Navigation";
 
 function App() {
   return (
-    <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
-
+    <BrowserRouter>
+      <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
